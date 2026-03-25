b702d25382523109ae18da6bac0c47f50b26f532
admin <admin@703-18>
2026-03-25 15:04:38 +0700

    Add project files.

---
+++ Enrollment.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolAppCore.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public float Grade { get; set; }
+    }
+}
