3122ad76152526076e70db67a24765e674fd0e43
Quoc Trung <doquoctrung2k@gmail.com>
2026-03-26 01:49:53 +0700

    completed ex2

---
+++ SchoolAppCoreMVC/SchoolAppCoreMVC/Models/SchoolContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace SchoolAppCoreMVC.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) :
+        base(options)
+        { }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public SchoolContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(local);Database=SchoolDB;Trusted_Connection=True;TrustServerCertificate=True");
+            }
+        }
+    }
+}
--- SchoolAppCoreMVC/SchoolAppCoreMVC/Program.cs
+++ SchoolAppCoreMVC/SchoolAppCoreMVC/Program.cs
@@ -1,8 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolAppCoreMVC.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<SchoolContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolDBConnectionString")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
